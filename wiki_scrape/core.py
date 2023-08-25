d7c5ef44abaa8d1de318f41c5b59eb395dba2f33
Ryan Bemowski <rrbemo@gmail.com>
2023-08-25 14:56:50 -0500

    Page 1 gets the data I'm looking for in a rough way. Sections are first gathered in a raw form and then cleaned int a non-raw form. Next step is getting all pages to run at once. Manually removed the "story character" page from the list of urls to keep from ending up with a really messy dataset.

--- wiki_scrape/core.py
+++ wiki_scrape/core.py
@@ -29,8 +29,9 @@ def get_page_data(page_url):
     page_info['Title'] = soup.find(id='firstHeading').get_text().strip()
 
     # Parse up to the TOC. Everything after the TOC should be based on the contents of the TOC.
-    for tag in soup.find(id='mw-content-text').find('div', class_='mw-parser-output').children:
-        #print(tag)
+    # I've tried to use .children here and it did not work properly
+    for tag in soup.find(id='mw-content-text').find('div', class_='mw-parser-output').find_all(recursive=False):
+        # print(tag)
         # Ignore navigable strings
         if isinstance(tag, bs.element.NavigableString):
             continue
@@ -38,56 +39,37 @@ def get_page_data(page_url):
             break
         # Skip the infobox
         if tag.name == 'div':
-            if 'class' in tag.attrs.keys() and 'infobox' not in tag['class']:
-                # Set the TOC to something else
-                if 'id' in tag.attrs.keys() and 'toc' in tag['id']:
-                    # This is the toc, let's store that for later parsing
-                    page_info['TOC'] = tag
-                # Else, just add it to the list of description items
-                else:
-                    if 'Description_Data' in page_info.keys():
-                        page_info['Description_Data'].append(tag)
-                    else:
-                        page_info['Description_Data'] = tag
-            else:
+            if 'class' in tag.attrs.keys() and 'infobox' in tag['class']:
                 # This is the infobox (multiple infoboxes will mess this up)
-                page_info['InfoBox'] = tag
-        # Else we will consider this as part of the description and add it.
-        elif 'Description_Data' in page_info.keys():
-            page_info['Description_Data'].append(tag)
-        else:
-            page_info['Description_Data'] = tag
+                page_info['InfoBox_RAW'] = tag # Set the TOC to something else
+            elif 'id' in tag.attrs.keys() and 'toc' in tag['id']:
+                # This is the toc, let's store that for later parsing
+                page_info['TOC_RAW'] = tag
+            # Else, just add it to the list of description items
+            else:
+                if 'Description_RAW' in page_info.keys():
+                    page_info['Description_RAW'].append(tag)
+                else:
+                    page_info['Description_RAW'] = tag
 
-        #     if 'spoiler-notice' in tag['class']:
-        #         # This is the overall spoiler text (states that there are spoilers and for what story)
-        #         if 'SpoilerAlert' in page_info.keys():
-        #             page_info['SpoilerAlert'].append(tag)
-        #         else:
-        #             page_info['SpoilerAlert'] = [tag]
-        # elif tag.name == 'table':
-        #     if 'expandable' in tag['class']:
-        #         # This is the actual spoiler hidden spoiler
-        #         if 'SpoilerText' in page_info.keys():
-        #             page_info['SpoilerText'].append(tag)
-        #         else:
-        #             page_info['SpoilerText'] = [tag]
-        # elif tag.name == 'p':
-            # This is the text for this section (description)
+        # Else we will consider this as part of the description and add it.
+        else:
+            if 'Description_RAW' in page_info.keys():
+                page_info['Description_RAW'].append(tag)
+            else:
+                page_info['Description_RAW'] = tag
 
     # Parse the remainder of the page based on the TOC.
     sections = {}
-    if 'TOC' in page_info.keys():
-        print("TOC FOUND")
+    if 'TOC_RAW' in page_info.keys():
         # Use the TOC to determine which sections we need to parse
-        for tag in page_info['TOC'].find('ul').children:
+        for tag in page_info['TOC_RAW'].find('ul').children:
             if isinstance(tag, bs.element.NavigableString):
                 continue
             if tag.name == 'li' and 'class' in tag.attrs.keys() and 'toclevel-1' in tag['class']:
-                #sections[tag['href']] = tag.get_text().strip()
                 sections[tag.find('a')['href'].replace("#", "")] = tag.find('span', class_='toctext').get_text().strip()
     else:
         # There is no TOC and we should loop through each h2 heading to determine what to do
-        print("NO TOC")
         for tag in soup.find('div', class_='mw-parser-output').children:
             if isinstance(tag, bs.element.NavigableString):
                 continue
@@ -96,25 +78,31 @@ def get_page_data(page_url):
                 headline = tag.find_next('span', class_='mw-headline')
                 if 'id' in headline.attrs.keys():
                     sections[headline['id']] = headline.get_text().strip()
-        print(sections)
 
     # Make a column for each section found (TOC or by h2 tag)
     for k in sections.keys():
+        section_raw = k + '_RAW'
         # Go to the beginning of that section
-        for tag in soup.find(id=k).next_siblings:
+        for tag in soup.find(id=k).parent.find_next_siblings():
             if isinstance(tag, bs.element.NavigableString):
-                continue
-            if tag.name == 'h2':
+                if section_raw in page_info.keys():
+                    page_info[section_raw].append(tag)
+                else:
+                    page_info[section_raw] = tag
+            elif tag.name == 'h2':
                 break
             else:
-                if k in page_info.keys():
-                    page_info[k].append(tag)
+                # Remove "edit" spans
+                for edit_tag in tag.find_all('span', class_='mw-editsection'):
+                    edit_tag.decompose()
+                if section_raw in page_info.keys():
+                    page_info[section_raw].append(tag)
                 else:
-                    page_info[k] = tag
+                    page_info[section_raw] = tag
 
     # parse infobox
-    if 'InfoBox' in page_info.keys():
-        ib = page_info['InfoBox']
+    if 'InfoBox_RAW' in page_info.keys():
+        ib = page_info['InfoBox_RAW']
         page_info['IB_Name'] = ib.find('p', class_='heading').get_text().strip()
         details = dict(zip([k.get_text().strip().replace(" ", "_") for k in ib.find_all('dt')],
                            [v for v in ib.find_all('dd')]))
@@ -130,31 +118,66 @@ def get_page_data(page_url):
                         multiples[child_count] = tag.get_text().strip()
                     #print(tag)
                     child_count += 1
-            print(k + ": " + str(child_count))
+            #print(k + ": " + str(child_count))
             if child_count > 1:
                 page_info['IB_' + k] = multiples
             else:
                 page_info['IB_' + k] = v.get_text().strip()
+        del page_info['InfoBox_RAW']
 
-    # Parse sections
-    if 'Description_Data' in page_info.keys():
-        desc_data = page_info['Description_Data']
-        for tag in desc_data.children:
+    # Parse description data
+    if 'Description_RAW' in page_info.keys():
+        desc_data = page_info['Description_RAW']
+        for tag in desc_data.find_all(recursive=False):
             if isinstance(tag, bs.element.NavigableString):
-                continue
-            print(tag)
-            if tag.name == 'blockquote':
+                if 'Description' in page_info.keys():
+                    page_info['Description'] += tag
+                else:
+                    page_info['Description'] = tag
+            elif tag.name == 'blockquote':
                 bq = tag.get_text().strip()
-                if 'Description_Quote' in page_info.keys():
-                    page_info['Description_Quote'].append(bq)
+                if 'Description_Quote_CLEAN' in page_info.keys():
+                    page_info['Description_Quote'] += '\n' + bq
                 else:
                     page_info['Description_Quote'] = bq
-            elif tag.name == 'p':
+            else: #if tag.name == 'p':
                 desc_text = tag.get_text().strip()
-                if 'Description' in page_info.keys():
-                    page_info['Description'].append(desc_text)
+                if 'Description_CLEAN' in page_info.keys():
+                    page_info['Description'] += '\n' + desc_text
                 else:
                     page_info['Description'] = desc_text
+        # Lastly, remove the RAW piece here so we don't run it again below:
+        del page_info['Description_RAW']
+
+    # Raw sections
+    raw_sections = [x for x in page_info.keys() if x.endswith('_RAW')]
+
+    # Parse description data
+    for section in raw_sections:
+        section_cleaned = section.replace('_RAW', '')
+        data = page_info[section]
+        for tag in data:
+            if isinstance(tag, bs.element.NavigableString):
+                if section_cleaned in page_info.keys():
+                    page_info[section_cleaned] += tag
+                else:
+                    page_info[section_cleaned] = tag
+            elif tag.name == 'div':
+                continue  # skip divs for now
+            else:  # if tag.name == 'p':
+                data_text = tag.get_text().strip()
+                if section_cleaned in page_info.keys():
+                    page_info[section_cleaned] += '\n' + data_text
+                else:
+                    page_info[section_cleaned] = data_text
+        # print(section_cleaned)
+        # print(page_info[section_cleaned])
+        # Remove the raw form from the dictionary
+        del page_info[section]
+
+
+
+    # Parse other sections
 
     return(page_info)
 
@@ -205,6 +228,8 @@ def get_story_involvement(page_url, page_soup):
 # Get all pages data (one page at a time)
 def get_data_from_pages(page_urls):
     page_infos = []
+    # Remove the "Story Characters" page if it is in the list of URLS. This is a circular page and creates clutter.
+    page_urls.remove('https://wiki.guildwars2.com/wiki/Story_characters')
     for page_url in page_urls:
         page_infos.append(get_page_data(page_url))
 
@@ -216,25 +241,15 @@ def main(root_path, category):
     pages = get_category_pages(root_path, category)
 
     # Get some info form the pages (this is pretty raw data that needs further parsing)
-    page_indecies = [2, 3, 71]
-    page_details = get_data_from_pages(pages[x] for x in page_indecies)
-
-    # Parse the infobox
-
-    # Parse section X
-
-    # Parse section Y... etc.
-
-    # page_details = get_data_from_pages(pages)
+    # page_indecies = [0, 1, 2, 3]
+    # less_pages = [pages[x] for x in page_indecies]
+    # page_details = get_data_from_pages(less_pages)
+    page_details = get_data_from_pages(pages)
     df = pd.DataFrame(page_details)
 
     # print("saving to file: /test_file.csv")
-    df.to_csv('test_file.csv', index = False, header = True)
+    df.to_csv('test_file.csv', index=False, header=True)
     print(df)
-    print(df.IB_Location[2])
-    print(df.IB_Race[2])
-    print(df.IB_Rank[2])
-    # print(df.TOC[2])
 
 if __name__ == "__main__":
     main('https://wiki.guildwars2.com', "story_characters")
